343182885f8caf80e2e42400a5bf63a7c420937e
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-06-26 14:15:38 +0530

    update and delete

--- client/addproduct.js
+++ client/addproduct.js
@@ -1,10 +1,10 @@
 var isEdit = false;
 var sel_id;
-window.onload = function(){
+window.onload = function () {
     getAllProducts();
 }
 
-async function getAllProducts(){
+async function getAllProducts() {
     let api = `http://localhost:3000/api/products/getAllProducts`;
     let response = await fetch(api);
     let result = await response.json();
@@ -12,16 +12,16 @@ async function getAllProducts(){
 }
 
 
-function showData(products){
+function showData(products) {
     let tbl = document.getElementById("ptable");
-    while(tbl.rows.length>1){
+    while (tbl.rows.length > 1) {
         tbl.deleteRow(1);
     }
-    for(let i=0;i<products.length;i++){
+    for (let i = 0; i < products.length; i++) {
         let e = products[i];
-        let cb = e.isActive?"checked":'';
+        let cb = e.isActive ? "checked" : '';
         let trow = `<tr>
-                <td>${i+1}</td>
+                <td>${i + 1}</td>
                 <td>${e.company}</td>
                 <td>${e.type}</td>
                 <td>${e.size}</td>
@@ -31,15 +31,27 @@ function showData(products){
                 <td><input type="checkbox" onchange="onStatus(event,'${e._id}')" ${cb}></td>
                 <td>
                     <button class="btn btn-info" onclick="onEdit('${e._id}')">Edit</button>
-                    <button class="btn btn-danger">Delete</button>
+                    <button class="btn btn-danger" onclick="onDelete('${e._id}')">Delete</button>
                 </td>
         </tr>`;
-        tbl.insertAdjacentHTML('beforeend',trow);
+        tbl.insertAdjacentHTML('beforeend', trow);
     }
 }
 
 
-async function onEdit(id){
+async function onDelete(id){
+if(confirm("Do you want to Delete?")){
+    let api = `http://localhost:3000/api/products/deleteProduct/${id}`;
+    let options = {
+        method: "DELETE"
+    }
+    let response = await fetch(api, options);
+    let result = await response.json();
+    getAllProducts();
+}   
+}
+
+async function onEdit(id) {
     let api = `http://localhost:3000/api/products/getProductById/${id}`;
     let response = await fetch(api);
     let result = await response.json();
@@ -56,7 +68,7 @@ async function onEdit(id){
     sel_id = id;
 }
 
-function onReset(){
+function onReset() {
     isEdit = false;
     document.getElementById("sbtn").innerHTML = "Add";
     document.getElementById("sbtn").className = "btn btn-primary";
@@ -64,54 +76,57 @@ function onReset(){
 }
 
 
-async function onStatus(e,id){
-let data = {
-    id:id,
-    isActive:e.target.checked
-}
-let api = `http://localhost:3000/api/products/updateProduct`;
-    let options = {
-        method:"PUT",
-        body:JSON.stringify(data),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
+function onStatus(e, id) {
+    let data = {
+        id: id,
+        isActive: e.target.checked
     }
-
-    let response = await fetch(api,options);
-    let result = await response.json();
-    console.log(result);
-    getAllProducts();
+    updateProduct(data);
 }
 
-function onSubmit(e){
+function onSubmit(e) {
     e.preventDefault();
     let product = {
-        company:document.getElementById("company").value,
-        type:document.getElementById("type").value,
-        size:document.getElementById("size").value,
-        price:document.getElementById("price").value,
-        model:document.getElementById("model").value,
-        color:document.getElementById("color").value
-    }
-    saveNewProduct(product);
+        company: document.getElementById("company").value,
+        type: document.getElementById("type").value,
+        size: document.getElementById("size").value,
+        price: document.getElementById("price").value,
+        model: document.getElementById("model").value,
+        color: document.getElementById("color").value
+    };
+    isEdit?(product.id=sel_id,updateProduct(product)):saveNewProduct(product);
 }
 
-async function saveNewProduct(product){
+async function updateProduct(data) {
+    let api = `http://localhost:3000/api/products/updateProduct`;
+    let options = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+    let response = await fetch(api, options);
+    let result = await response.json();
+    getAllProducts();
+    onReset();
+    document.getElementById("pForm").reset();
+}
+
+async function saveNewProduct(product) {
     let api = `http://localhost:3000/api/products/saveNewProduct`;
     let options = {
-        method:"POST",
-        body:JSON.stringify(product),
+        method: "POST",
+        body: JSON.stringify(product),
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
     }
 
-    let response = await fetch(api,options);
+    let response = await fetch(api, options);
     let result = await response.json();
-    console.log(result);
     getAllProducts();
     document.getElementById("pForm").reset();
 
